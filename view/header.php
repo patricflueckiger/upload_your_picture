da73e899bd0e1d8f13ab8240171f9169e5fb682a
Barbarossa Gabriele <bbarbg@bbc-be.local>
2017-10-17 16:00:29 +0200

    nav design angepasst

--- view/header.php
+++ view/header.php
@@ -8,8 +8,7 @@
     <title><?= $title ?> | Bbc MVC</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="/css/style.css" rel="stylesheet">
 
@@ -20,24 +19,21 @@
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-light bg-light">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">Bbc MVC</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/user">Benutzer</a></li>
-            <li><a href="/user/create">Benutzer erstellen</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
+        <a class="btn btn-primary" href="#" role="button">Upload</a>
+        <a class="navbar-brand" href="/">Upload Your Picture</a>
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/user">User</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/user/create">User erstellen</a>
+          </li>
+        </ul>
       </div>
     </nav>
 
