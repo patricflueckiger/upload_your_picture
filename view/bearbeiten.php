e092d8b3648cd1c3e18b988b8b2dfcde4042984f
Barbarossa Gabriele <bbarbg@bbc-be.local>
2017-10-24 10:46:37 +0200

    edit funciotn hinzugefügt beim bildercontroller

---
+++ view/bearbeiten.php
@@ -0,0 +1,6 @@
+<div class="pt-5" style="background-color:#d0e4f7;height:100%;width:auto;border-radius:5px;">
+  <div class="container">
+    <h3 class="text-center">Bild Bearbeiten</h3>
+    <img src="" alt="">
+  </div>
+</div>
