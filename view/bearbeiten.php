76e882b492f6f1ba3e23416df645f7a2bd57e96a
Barbarossa Gabriele <bbarbg@bbc-be.local>
2017-10-30 12:45:07 +0100

    beim bearbeiten werden die felder mit JS überprüft

--- view/bearbeiten.php
+++ view/bearbeiten.php
@@ -1,9 +1,10 @@
 <div class="pt-5" style="background-color:#d0e4f7;height:100%;width:auto;border-radius:5px;">
   <div class="container">
+    <h4 id="fehler-meldung" style="color:red;">
     <?php
     if(isset($_GET['error'])){
     ?>
-    <h4 id="fehler-meldung" style="color:red;"><?php
+    <?php
     switch ($_GET['error']) {
       default:
         echo "Alle Felder müssen ausgefüllt sein!";
@@ -21,17 +22,17 @@
 
     <div class="form-group" style="float:left; width: 50%">
       <label for="inputTitel">Titel:</label>
-      <input type="text" name="inputTitel" id="inputTitel" placeholder="Titel" class="form-control" value="<?php echo $bild->titel?>" required>
+      <input type="text" name="inputTitel" id="inputTitel" placeholder="Titel" class="form-control input" value="<?php echo $bild->titel?>" required>
     </div>
 
     <div class="form-group" style="display:inline-block; width: 50%;">
       <label for="inputOrt">Ort:</label>
-      <input type="text" name="inputOrt" id="inputOrt" placeholder="Ort" class="form-control" value="<?php echo $bild->ort?>" required>
+      <input type="text" name="inputOrt" id="inputOrt" placeholder="Ort" class="form-control input" value="<?php echo $bild->ort?>" required>
     </div>
 
     <div >
       <label for="inputBeschreib">Beschreibung:</label><br>
-      <textarea rows="5" cols="40" name="inputBeschreib"   id="inputBeschreib" class="form-control" required><?php echo $bild->beschreibung?></textarea>
+      <textarea rows="5" cols="40" name="inputBeschreib"   id="inputBeschreib" class="form-control input" required><?php echo $bild->beschreibung?></textarea>
     </div>
 
     <div class="form-check mt-2">
@@ -57,7 +58,7 @@
         <div class="col-lg-6">
           <!-- $_SERVER['HTTP_REFERER']; für den letzt aufgerufe URL zu getten -->
           <a class="btn btn-secondary" href="<?php echo $_SERVER['HTTP_REFERER']; ?>" role="button">Zurück</a>
-          <input type="submit" name="inputButton" id="inputButton" value="Speichern" class="btn btn-primary">
+          <input type="submit" onclick="validateInput()" name="inputButton" id="inputButton" value="Speichern" class="btn btn-primary">
         </div>
         <div class="col-lg-6">
           <div class="text-right">
